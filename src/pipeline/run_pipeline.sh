f028422ecbfbaf248f48e999e9af63f14ff121a2
thingphan <kmg98839883@gmail.com>
2026-06-07 20:40:59 +0900

    Update run_pipeline.sh

--- src/pipeline/run_pipeline.sh
+++ src/pipeline/run_pipeline.sh
@@ -32,7 +32,12 @@ echo "------------------------------------------------------"
 echo "[5] Hive로 Top 100 리포트 추출"
 export HADOOP_CLIENT_OPTS="-Dfile.encoding=UTF-8"
 # 하이브 쿼리 실행 후 결과를 로컬 CSV로 저장
-hive --silent=true --showHeader=true --outputformat=csv2 -e "SELECT * FROM final_transit_blind_spot WHERE distance_km >= 1.0 ORDER BY total_living_pop DESC, daily_total_on DESC LIMIT 100;" > final_top100_report.csv
+hive --silent=true --showHeader=true --outputformat=csv2 -e \
+    "SELECT *, (total_living_pop * daily_total_on) AS synergy_score \
+    FROM final_transit_blind_spot \
+    WHERE distance_km >= 1.0 \
+    ORDER BY synergy_score DESC \
+    LIMIT 100;" > final_top100_report.csv
 
 # 최종 리포트를 HDFS에 백업
 hadoop fs -put -f final_top100_report.csv /user/maria_dev/project/
